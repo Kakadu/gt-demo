83ca0632affd4a3f6adac718365edc40335a4bc1
kakadu <kakadu.hafanana@gmail.com>
2018-02-01 21:16:55 +0300

    Test with non-rec works

--- src/demo09nonrec.ml
+++ src/demo09nonrec.ml
@@ -4,41 +4,22 @@ module P = struct
     method virtual c_P   : 'inh -> 'a -> 'syn
   end
 
-  let gcata_t tr inh t = function
+  let gcata_t tr inh = function
   | P   n -> tr#c_P   inh n
 
   class ['a] show_t self fa = object
     inherit ['a, unit, string, unit, string] class_t
-    method c_P () n = "P " ^ (fa n)
+    method c_P () n = Format.sprintf "P %a" fa n
   end
 
-  let show_t fa t = Utils.fix0 (fun self -> gcata_t (new show_t self fa) ()) t
+  let show_t fa () t = Utils.fix0 (fun self -> gcata_t (new show_t self fa)) () t
 end
 
 open Demo04option
 open P
 
-(* AIM: to generate something for type nonrec 'a t = 'a t option *)
-
-(* and now it is either impossible to generate  code .... *)
-(* type nonrec 'a t = 'a t option
- *
- * class virtual ['a, 'ia, 'sa, 'inh, 'syn] class_t =
- *   object
- *     inherit ['a P.t, 'ia P.t, 'sa P.t, 'inh, 'syn] class_toption
- *     (* by default we use 'a t and not 'a P.t in the line above *)
- *   end
- * class ['a] show_t self fa =
- *   object
- *     inherit ['a, unit, string, unit, string] class_t
- *     inherit ['a P.t] show_toption self
- *         (fun l -> show_t fa l)
- *   end *)
-
-
-(* ... or we can think that for aliases we can generate classes _before_ actual type
-   declaration but it will no type check *)
-
+(* The idea is to put type declaration after class declaration to make
+   previous type `t` visible *)
 class virtual ['a, 'ia, 'sa, 'inh, 'syn] class_t =
   object
     inherit ['a t, 'ia t, 'sa t, 'inh, 'syn] class_toption
@@ -47,11 +28,29 @@ class ['a] show_t self fa =
   object
     inherit ['a, unit, string, unit, string] class_t
     inherit ['a t] show_toption self
-        (fun l -> show_t fa l)
+        (fun () l -> show_t fa () l)
   end
-let gcata_t = gcata_t
+let gcata_t = gcata_toption
 (* will not typecheck *)
 let show_t fa t = Utils.fix0 (fun self -> gcata_t (new show_t self fa) ()) t
 type nonrec 'a t = 'a t option
 let () =
-  Printf.printf "%s\n%!" @@ show_t id (Some (P "ppp"))
+  Printf.printf "%s\n%!" @@ show_t (fun () -> Utils.id) (Some (P "ppp"))
+
+module Experiment2 = struct
+  (* Now let's make the same trick with non-alias type *)
+  open P
+
+  class virtual ['inh, 'syn] class_t = object
+    method virtual c_Foo : 'inh -> int t -> 'syn
+  end
+  class show_t self = object
+    inherit [unit, string] class_t
+    method c_Foo () x = Printf.sprintf "Foo(%s)" (show_t (fun () -> string_of_int) () x)
+  end
+  type nonrec t = Foo of int t
+  let gcata_t tr inh = function Foo x -> tr#c_Foo inh x
+  let show_t () t = Utils.fix0 (fun self -> gcata_t (new show_t self)) () t
+  let () =
+    Printf.printf "%s\n%!" @@ show_t ()  (Foo (P 25))
+end

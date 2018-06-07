d8646b188ad685edc9ce3579c358ffd7a813da5f
kakadu <kakadu.hafanana@gmail.com>
2018-06-08 01:26:31 +0300

    WIP on extensibles

--- src/demo11ext.ml
+++ src/demo11ext.ml
@@ -1,5 +1,13 @@
 type expr = ..
 
+class virtual ['inh, 'self, 'syn] expr_t = object
+
+end
+
 type expr +=
   | Const of int
   | Add of expr * expr
+
+class ['inh, 'self, 'syn] expr_t = object
+  inherit ['inh, 'self, 'syn] expr_t
+end

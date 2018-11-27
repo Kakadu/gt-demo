8219692eec932f3454c3744c163c1d2c1947f7c8
kakadu <kakadu.hafanana@gmail.com>
2018-11-27 13:48:54 +0300

    It seems that everything works

--- src/demo02polyvars.ml
+++ src/demo02polyvars.ml
@@ -290,7 +290,7 @@ end = struct
       match `D (fa a) with #pv_ext2 as x -> x
   end
 
-  let rec gcata_pv_ext2 tr inh s =
+  let gcata_pv_ext2 tr inh s =
     match s with
     | `D a -> tr#c_D inh s a
     | #pv_ext as subj -> gcata_pv_ext tr inh subj
@@ -319,3 +319,134 @@ let _ =
   Printf.printf "Mapped PV_ext2 and showed as a pv_ext2: %s\n" @@
     show_pv_ext2 string_of_int  id
       (gmap_pv_ext2 int_of_string id (`D "1"));
+
+
+
+(* ********************************** Completely another type *********************** *)
+module PV2 : sig
+  type 'a pv2 = [ `F of 'a ]
+
+  class virtual ['ia, 'a, 'sa, 'inh, 'self, 'syn] class_pv2_t : object
+    method virtual c_F : 'inh -> 'a pv2 -> 'a -> 'syn
+  end
+  class ['a] show_pv2_t :
+    ('a -> string) ->
+    ( 'a pv2 -> string) ->
+    object
+      inherit [ unit, 'a, string, unit, 'self, string] class_pv2_t
+      method c_F : unit -> 'a pv2 -> 'a -> string
+    end
+  class ['a, 'a1, 'self] gmap_pv2_t :
+    ('a -> 'a1) ->
+    ('a pv2 -> 'self) ->
+    object
+      constraint 'self = [> 'a1 pv2 ]
+      inherit [ unit, 'a, 'a1
+              , unit, 'self, 'self] class_pv2_t
+      method c_F : unit -> 'a pv2 -> 'a -> 'self
+    end
+
+  val gcata_pv2 :
+    (_,'a,'syn, 'inh, _, 'syn) #class_pv2_t ->
+    'inh -> 'a pv2 -> 'syn
+
+  val show_pv2 :
+    ('a -> string) -> 'a pv2 -> string
+
+  (* We add more subtyping polymorphism to the result type to avoid exlicit casts
+   * when using the result of gmap_pv as an argument of show_pv_ext *)
+  val gmap_pv2 : ('a -> 'b) -> 'a pv2 -> [> 'b pv2 ]
+end = struct
+  type 'a pv2 = [ `F of 'a ]
+
+  class virtual ['ia, 'a, 'sa, 'inh, 'self, 'syn] class_pv2_t =
+  object
+    method virtual c_F : 'inh -> 'a pv2 -> 'a -> 'syn
+  end
+
+  let gcata_pv2 tr inh (s : _ pv2) = match s with
+    | `F a -> tr#c_F inh s a
+
+  class ['a] show_pv2_t fa fself = object
+    inherit [unit, 'a, string, unit, 'self, string] class_pv2_t
+    method c_F () _ a = sprintf "`F (%s)" (fa a)
+  end
+
+  (* I added an self argument for the 'self variable in the open type
+   * We need this in gmap class and not in a show implementation
+  *)
+  class ['a, 'a1, 'self ] gmap_pv2_t (fa : 'a -> 'a1) fself =
+  object
+    inherit [ unit, 'a, 'a1
+            , unit, 'self, [> 'a1 pv2 ] as 'self] class_pv2_t
+    method c_F () _ a = match `F (fa a) with #pv2 as x -> x
+  end
+
+  let pv2 = { gcata = gcata_pv2 }
+
+  let show_pv2 fa t = transform(pv2) (new show_pv2_t fa) t
+  let gmap_pv2 fa t = transform(pv2) (new gmap_pv2_t fa) t
+end
+
+let _ =
+  let open PV2 in
+  Printf.printf "Original PV: %s\nMapped PV: %s\n"
+      (show_pv2 id            (`F "1"))
+      (show_pv2 string_of_int @@
+       gmap_pv2 int_of_string  (`F "1"))
+
+
+(* ********************************** Union of PV & PV2  *********************** *)
+
+module PVSum = struct
+  type ('a,'b) s = [ ('a,'b) PV.pv | 'a PV2.pv2 ]
+
+  class virtual ['ia, 'a, 'sa, 'ib, 'b, 'sb, 'inh, 'self, 'syn] class_s_t =
+  object
+    inherit ['ia, 'a, 'sa, 'ib, 'b, 'sb, 'inh, 'self, 'syn]  PV.class_pv
+    inherit ['ia, 'a, 'sa              , 'inh, 'self, 'syn] PV2.class_pv2_t
+  end
+
+  class ['a, 'b] show_s_t fa fb fself =
+    object
+      inherit [ unit, 'a, string, unit, 'b, string, unit, 'self, string] class_s_t
+      inherit ['a, 'b]  PV.show_pv_t fa fb (function  #PV.pv  as s -> fself s)
+      inherit ['a]      PV2.show_pv2_t  fa (function #PV2.pv2 as s -> fself s)
+    end
+
+  class ['a, 'a1, 'b, 'b1, 'self ] gmap_s_t fa fb  fself =
+  object
+    inherit [ unit, 'a, 'a1
+            , unit, 'b, 'b1
+            , unit, 'self, [> ('a1,'b1) s ] as 'self] class_s_t
+    inherit ['a, 'a1, 'b, 'b1, 'self ]  PV.gmap_pv_t  fa fb (function #PV.pv as s -> fself s)
+    inherit ['a, 'a1,          'self ] PV2.gmap_pv2_t fa (function #PV2.pv2 as s -> fself s)
+  end
+
+  let gcata_s tr inh s =
+    match s with
+    |  #PV.pv  as subj -> PV.gcata_pv tr inh subj
+    | #PV2.pv2 as subj -> PV2.gcata_pv2 tr inh subj
+
+  let s = { gcata = gcata_s }
+  let show_s fa fb subj =
+    transform0(s) (new show_s_t fa fb) subj
+
+  let gmap_s fa fb subj =
+    transform0(s) (new gmap_s_t fa fb) subj
+
+end
+
+let _ =
+  let open PVSum in
+
+  let f x =
+    Printf.printf "Original PV: %s\nMapped PV: %s\n"
+      (show_s id            id x)
+      (show_s string_of_int id @@
+       gmap_s int_of_string id x)
+  in
+
+  f (`F "1");
+  f (`A "2");
+  f (`B "3");
